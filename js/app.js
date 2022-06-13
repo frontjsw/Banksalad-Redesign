0863bd632fd73899575bbf3862481092c388abf0
frontjsw <89008053+frontjsw@users.noreply.github.com>
2022-06-13 09:04:26 +0900

    content2 추가

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-window.onload = function () {
-  const con1Title = document.querySelector(".content1 .left_area h1");
-  window.addEventListener("scroll", () => {
-    const con1Top = document.querySelector(".content1 .left_area").offsetTop;
-    if (window.scrollY > con1Top) {
-      con1Title.classList.add("on");
-    } else {
-      con1Title.classList.remove("on");
-    }
-  });
-};
+// content1 fixed 액션
+const con1Title = document.querySelector(".content1 .left_area h1");
+const con1TitleH = con1Title.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const con1Top = document.querySelector(".content1").offsetTop;
+  const con1EndH = document.querySelector(".all_btn").offsetTop - con1TitleH;
+  if (window.scrollY > con1Top && window.scrollY < con1EndH) {
+    con1Title.classList.add("on");
+    con1Title.classList.remove("end");
+  } else if (window.scrollY > con1EndH) {
+    con1Title.classList.remove("on");
+    con1Title.classList.add("end");
+  } else {
+    con1Title.classList.remove("on");
+  }
+});
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -1,14 +1,19 @@
 "use strict";
 
-window.onload = function () {
-  var con1Title = document.querySelector(".content1 .left_area h1");
-  window.addEventListener("scroll", function () {
-    var con1Top = document.querySelector(".content1 .left_area").offsetTop;
+// content1 fixed 액션
+var con1Title = document.querySelector(".content1 .left_area h1");
+var con1TitleH = con1Title.clientHeight;
+window.addEventListener("scroll", function () {
+  var con1Top = document.querySelector(".content1").offsetTop;
+  var con1EndH = document.querySelector(".all_btn").offsetTop - con1TitleH;
 
-    if (window.scrollY > con1Top) {
-      con1Title.classList.add("on");
-    } else {
-      con1Title.classList.remove("on");
-    }
-  });
-};
+  if (window.scrollY > con1Top && window.scrollY < con1EndH) {
+    con1Title.classList.add("on");
+    con1Title.classList.remove("end");
+  } else if (window.scrollY > con1EndH) {
+    con1Title.classList.remove("on");
+    con1Title.classList.add("end");
+  } else {
+    con1Title.classList.remove("on");
+  }
+});
